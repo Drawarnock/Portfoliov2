62f012d9451f033ae126a724bddd453a2a524455
Konrad Wisłocki <wislockik23@gmail.com>
2018-04-12 01:17:03 +0200

    RWD in footer, contact, header

--- src/js/main.js
+++ src/js/main.js
@@ -5,4 +5,34 @@
             if(body.classList.contains('navigation--active')) body.classList.remove('navigation--active');
             else body.classList.add('navigation--active');
         });
+
+
+        const links = document.querySelectorAll('.navigation__link[href^="#"]');
+        const speed = 500;
+        const movingFreq = 15;
+       
+        links.forEach(link => link.addEventListener('click', function(e) {
+            e.preventDefault();
+            let href;
+            if(this.getAttribute('href').length>1)
+            {
+                href = this.getAttribute('href').toString();
+            } else {
+                return;
+            }
+            const targetElement = document.getElementById(href.slice(1));
+            const documentScroll = window.scrollY;
+            const targetElementOffset = targetElement.offsetTop;
+            const stepCount = Math.floor(speed/movingFreq);
+            const stepCountGap = (targetElementOffset - documentScroll)/stepCount;
+          
+            for (var i = 1; i <= stepCount; i++)
+            {
+                (function()
+                {
+                    let stepPosition = stepCountGap * i;
+                    setTimeout(() => window.scrollTo(0, stepPosition + documentScroll), movingFreq * i);
+                })();
+            }
+        }));
  })();
